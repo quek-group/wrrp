07f969111a24557dcd45b8f623df371c7b78cb9b
Keian Noori <keian.noori@gmail.com>
2019-06-18 19:01:19 +0800

    test

--- cplx.sh
+++ cplx.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Compile complex version of the code
 
-
 rm *mod
 #ifort -O3 -fpp -DCPLX -xHost -qopt-matmul -qopenmp -parallel -traceback -o wrrp.cplx.sc.x params.f90 v.f90 mergesort.f90 supercell.f90 fftw.f90 input.f90  wrrp.f90 -I/opt/intel/mkl/include/fftw/ -L/opt/intel/mkl/lib/intel64/ -lfftw2xf_double_intel -mkl
 ifort -O3 -fpp -DCPLX -xHost -qopt-matmul -qopenmp -parallel -traceback -o wrrp.cplx.sc.x params.f90 v.f90 mergesort.f90 supercell.f90 fftw.f90 input.f90  wrrp.f90  -lfftw2xf_double_intel $LIBLAPACK_MT
