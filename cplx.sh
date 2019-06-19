449bd93a8a6d2c10e26779fb059af042845b8e6f
Keian Noori <keian.noori@gmail.com>
2019-06-19 16:45:48 +0800

    Remove fftw3.f90 (no benefit), add BGW patch, update compile scripts

--- cplx.sh
+++ cplx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Compile complex version of the code
+# An example script to compile the COMPLEX version of the code
+# We compile with LAPACK, FFTW2, and OpenMP
+# Note that you will need to modify this script to suit your machine's environment
 
- 
 rm *mod
-#ifort -O3 -fpp -DCPLX -xHost -qopt-matmul -qopenmp -parallel -traceback -o wrrp.cplx.sc.x params.f90 v.f90 mergesort.f90 supercell.f90 fftw.f90 input.f90  wrrp.f90 -I/opt/intel/mkl/include/fftw/ -L/opt/intel/mkl/lib/intel64/ -lfftw2xf_double_intel -mkl
 ifort -O3 -fpp -DCPLX -xHost -qopt-matmul -qopenmp -parallel -traceback -o wrrp.cplx.sc.x params.f90 v.f90 mergesort.f90 supercell.f90 fftw.f90 input.f90  wrrp.f90  -lfftw2xf_double_intel $LIBLAPACK_MT
-#ifort -O3 -fpp -DCPLX -xHost -qopt-matmul  -traceback -check all -o wrrp.cplx.sc.x params.f90 v.f90 mergesort.f90 supercell.f90 fftw.f90 input.f90  wrrp.f90  -lfftw2xf_double_intel $LIBLAPACK_MT
+#ifort -O3 -fpp -DCPLX -xHost -qopt-matmul -qopenmp -parallel -traceback -o wrrp.cplx.sc.x params.f90 v.f90 mergesort.f90 supercell.f90 fftw.f90 input.f90  wrrp.f90 -I/opt/intel/mkl/include/fftw/ -L/opt/intel/mkl/lib/intel64/ -lfftw2xf_double_intel -mkl
--- real.sh
+++ real.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# Compile complex version of the code
-
-ifort -O3 -fpp -qopt-matmul -qopenmp -parallel -o wrrp.real.x wrrp.f90 input.f90 params.f90 fftw.f90 v.f90 -I/opt/intel/mkl/include/fftw/ -L/opt/intel/mkl/lib/intel64/ -lfftw2xf_double_intel -mkl
+# An example script to compile the REAL version of the code
+# We compile with LAPACK, FFTW2, and OpenMP
+# Note that you will need to modify this script to suit your machine's environment
+ 
+rm *mod
+ifort -O3 -fpp -xHost -qopt-matmul -qopenmp -parallel -traceback -o wrrp.cplx.sc.x params.f90 v.f90 mergesort.f90 supercell.f90 fftw.f90 input.f90  wrrp.f90  -lfftw2xf_double_intel $LIBLAPACK_MT
+#ifort -O3 -fpp -xHost -qopt-matmul -qopenmp -parallel -traceback -o wrrp.cplx.sc.x params.f90 v.f90 mergesort.f90 supercell.f90 fftw.f90 input.f90  wrrp.f90 -I/opt/intel/mkl/include/fftw/ -L/opt/intel/mkl/lib/intel64/ -lfftw2xf_double_intel -mkl
